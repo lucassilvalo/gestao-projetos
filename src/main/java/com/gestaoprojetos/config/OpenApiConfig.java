71b2950c6403f25003285952dcd88f736d2d4338
unknown <lucasjwlopes@outlook.com>
2025-09-28 23:19:48 -0300

    Adicionada configuração do Swagger/OpenAPI com suporte a Basic Auth

--- src/main/java/com/gestaoprojetos/config/OpenApiConfig.java
+++ src/main/java/com/gestaoprojetos/config/OpenApiConfig.java
@@ -1,17 +1,31 @@
 package com.gestaoprojetos.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class OpenApiConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "basicAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Gestão de Projetos API")
-                        .version("1.0")
-                        .description("Documentação da API para gerenciamento de projetos"));
+                        .version("v1")
+                        .description("API para gerenciar usuários e projetos"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("basic")));
     }
-
 }
